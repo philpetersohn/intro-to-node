202175e9366bf91832a494fe023e1954cc28d38e
philpetersohn <petersohn.phil@gmail.com>
2024-12-17 14:28:09 -0800

    Finished node introduction

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ export const listNotes = (notes) => {
   notes.forEach((note) => {
     console.log("\n");
     console.log("id: ", note.id);
-    console.log("tags: ", note.tags.join(", "));
-    console.log("note: ", note.content);
+    console.log("tags: ", note.tags.join(", ")),
+      console.log("note: ", note.content);
   });
 };
